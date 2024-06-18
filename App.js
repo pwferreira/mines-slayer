9e62bae817134bbb124ebdc3037984fbf8beb8b0
Patrick Ferreira <pwferreira.info@gmail.com>
2024-06-17 22:44:55 -0300

    Mine Field e mais logica no functions.js

--- App.js
+++ App.js
@@ -1,30 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Field from './src/components/Field';
-import Flag from './src/components/Flag';
+import { StyleSheet, Text, View, Alert} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Field></Field>
-      <Field opened></Field>
-      <Field opened nearMines={1}/>
-      <Field opened nearMines={2}/>
-      <Field opened nearMines={3}/>
-      <Field opened nearMines={6}/>
-      <Field mined/>
-      <Field mined opened/>
-      <Field mined opened exploded/>
-      <Field flagged/> 
+import MineField from './src/components/MineField';
+import params from './src/params';
+import { createMinedBoard, cloneBoard, hadExplosion, openField, showMines, wonGame } from './src/functions';
+import { Component } from 'react';
 
-    </View>
-  );
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = this.createState();
+  }
+
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount())
+    }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board);
+    openField(board, row, column);
+    const lost = hadExplosion(board);
+    const won = wonGame(board);
+   if(lost){
+      showMines(board);
+      Alert.alert('Perdeeuuuu','Que pena');
+   }
+   if(won){
+      Alert.alert('Ganhouuuu','Parabéns');
+   }
+   
+   this.setState({ board, lost, won});
+ }
+
+
+ render(){
+    return (
+      <View style={styles.container}>
+        <View style= {styles.board}>
+           <MineField board={this.state.board} onOpenField={this.onOpenField}/>
+        </View>
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFFr',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+
+  }
+
 });
